2f66353341528311e0cc3636363c991576298162
James Home <jamesfoy17>
2019-09-24 14:33:38 +0100

    Added Prototype Crop Select Wheel
    When the player clicks on a plot of land a selection wheel apears allowing the player to click on one of the 6 options of "crops"

---
+++ Assets/Scripts/Plot/PlotInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotInteraction : MonoBehaviour
+{
+    [SerializeField]
+    GameObject SelectWheel;
+
+    void OnMouseEnter()
+    {
+        //Highlight/change image
+        Debug.Log("Mouse Entered Plot");
+    }
+
+    public void OnButtonClick()
+    {
+        SelectWheel.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        SelectWheel.SetActive(true);
+    }
+}
