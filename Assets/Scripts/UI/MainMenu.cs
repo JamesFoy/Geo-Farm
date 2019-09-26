4856bd16b8f36aa7776b748868552c641ae4b461
WES15004515 <45922289+WES15004515@users.noreply.github.com>
2019-09-26 13:39:54 +0100

    Main Menu
    Added a main menu which finds the main scene and loads it. Also allows the player to quit the game when built.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenu : MonoBehaviour {
+
+    public string playFarmingSim; //Accesses the Farming Sim scene
+
+	public void PlayGame() //Starts the game by opening the Farming Sim scene
+    {
+        SceneManager.LoadScene(playFarmingSim); //Loads the scene known as Farming Sim
+    }
+
+    public void QuitGame() //Closes the game to desktop 
+    {
+        Application.Quit(); //The game stops running and closes
+    }
+}
