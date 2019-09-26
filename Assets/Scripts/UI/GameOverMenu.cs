5c1a70e5308ea34fb2542544f00859caa43eb340
WES15004515 <45922289+WES15004515@users.noreply.github.com>
2019-09-26 16:58:28 +0100

    Added necessary scripts to the return to menu and restart buttons. Currently a prototype, still need to figure out how to make the menu activate when timer is up and how to hide the menu when the restart button is pressed

---
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameOverMenu : MonoBehaviour {
+
+    public string mainMenuLevel; //Allows the script to access the Main Menu scene
+
+    public void QuitToMain() //Loads the Main Menu scene
+    {
+        SceneManager.LoadScene(mainMenuLevel); //Loads the scene known as Main Menu
+    }
+
+}
---
+++ Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    public void RestartGame() //Restarts the current scene
+    {
+        SceneManager.LoadScene("Farming Sim");
+    }
+}
